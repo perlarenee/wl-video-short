e2ac6099908cec2185fa953f24db3da9e6a1a46a
Renee <info@perlarenee.com>
2016-06-22 01:53:56 -0500

    Small adj

--- wl-video-short.php
+++ wl-video-short.php
@@ -139,4 +139,4 @@ function videoshort_function($atts, $content = null){
 	return $output;
 }
 add_shortcode('videoshort', 'videoshort_function' );
-//USE: [videoshort id="anID" video_id="171439497" service="vimeo" video_height="360" video_width="640"  max_height="500" min_height="300" limit_width="true" max_width="1000"  thumb_url="http://fullurl.jpg" thumb_alt="Alt text" thumb_back="#FFFFFF" video_back="#333333" crop_direction="center" video_overlay="Some overlay text" click_label="Click Here" autoplay="true" repeat="true" loop="true" ][/videoshort]
+//USE: [videoshort id="anID" video_id="171439497" service="vimeo" video_height="360" video_width="640"  max_height="500" min_height="300" limit_width="true" max_width="1000"  thumb_url="http://fullurl.jpg" thumb_alt="Alt text" thumb_back="#FFFFFF" video_back="#333333" crop_direction="center" video_overlay="Some overlay text" click_label="Click Here" autoplay="true" repeat="true"][/videoshort]
